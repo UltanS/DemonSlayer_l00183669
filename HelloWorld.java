ae35823d1aee113cb89375e47a647aa70aef9642
UltanS <ultandoherty04@gmail.com>
2025-10-15 13:00:25 +0100

    Added the file HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+    Name: HelloWorld.java
+    Student ID: l00183669
+    Description: A simple Java Program, this program tests the pipeline setup.
+*/
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Instance variables
+        Scanner keyIn = new Scanner(System.in);
+        String user_name;
+
+        // Get users name
+        System.out.print("Enter your name: ");
+        user_name = keyIn.nextLine();
+
+        // Print Hello to the user
+        System.out.println("Hello " + user_name + "!");
+    }
+}
